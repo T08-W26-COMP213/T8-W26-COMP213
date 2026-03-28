b7bd7db902cf16b62288d12e8153fd61df679d0f
visionbaral2 <vbaral@my.centennialcollege.ca>
2026-03-28 11:46:28 -0400

    Fix CORS

--- backend/server.js
+++ backend/server.js
@@ -9,15 +9,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors({
-  origin: [
-    "http://localhost:5173",
-    "https://fanciful-dusk-c3a5b0.netlify.app",
-    "https://stock-guard1.netlify.app"
-  ],
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  credentials: true
-}));
+app.use(cors());
 
 app.use(express.json());
 
